6a3eaab01dc7a006d640f721be26509d6193fcd5
DiMiRka <fallen_94@bk.ru>
2025-09-29 15:21:27 +0700

    start

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplotlib
+from matplotlib import pyplot as plt
+import seaborn as sns
+import tempfile
+import os
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
