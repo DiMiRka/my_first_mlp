edc4f37a078f334a22e1956c63c542210672360c
DiMiRka <fallen_94@bk.ru>
2025-09-29 15:24:44 +0700

    start2

--- main.py
+++ main.py
@@ -1,9 +1,158 @@
-import pandas as pd
-import matplotlib
-from matplotlib import pyplot as plt
-import seaborn as sns
-import tempfile
 import os
+import tempfile
+
+import matplotlib
+import pandas as pd
+import seaborn as sns
 import tensorflow as tf
 import tensorflow_datasets as tfds
+from matplotlib import pyplot as plt
 
+
+class DenseLayer(tf.Module):
+    def __init__(self, out_dim, weight_init=tf.keras.initializers.GlorotUniform, activation=tf.identity):
+        # Инициализация размеров и функции активации
+        self.out_dim = out_dim  # Определяем размерность выходов
+        self.weight_init = weight_init  # Инициализируем веса
+        self.activation = activation  # Задаем функцию активации
+        self.built = False
+
+    @tf.function  # Декоратор, который переносит вычисления на уровень графа TensorFlow
+    def __call__(self, x):
+        if not self.built:  # Если запущен первый раз проводим инициализацию
+            # Вывод входного измерения на основе первого вызова
+            self.in_dim = x.shape[1]
+            # Инициализация весов и смещений
+            self.w = tf.Variable(self.weight_init(shape=(self.in_dim, self.out_dim)))  # Объявление тензорных переменных
+            self.b = tf.Variable(tf.zeros(shape=(self.out_dim,)))  # Объявление тензорных переменных
+            self.built = True  # Инициализация пройдена
+        # Вычисление прямого прохода
+        y = tf.add(tf.matmul(x, self.w), self.b)  # уравнение персептрона
+        return self.activation(y)  # Применение функции активации к уравнению персептрона
+
+
+class MLP(tf.Module):
+    def __init__(self, layers):
+        self.layers = layers
+
+    @tf.function  # Декоратор, который переносит вычисления на уровень графа TensorFlow
+    def __call__(self, x, preds=False):
+        # Последовательное выполнение слоев модели
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+
+hidden_layer_1_size = 700
+hidden_layer_2_size = 500
+hidden_layer_3_size = 300
+hidden_layer_4_size = 100
+output_size = 10
+
+# Создание модели нейронной сети
+mlp_model = MLP([
+    DenseLayer(out_dim=hidden_layer_1_size, activation=tf.nn.relu),
+    DenseLayer(out_dim=hidden_layer_2_size, activation=tf.nn.relu),
+    DenseLayer(out_dim=hidden_layer_3_size, activation=tf.nn.relu),
+    DenseLayer(out_dim=hidden_layer_4_size, activation=tf.nn.relu),
+    DenseLayer(out_dim=output_size)])
+
+
+def cross_entropy_loss(y_pred, y):
+    # Вычисление перекрестной энтропии с помощью разреженной операции
+    sparse_ce = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=y_pred)
+    return tf.reduce_mean(sparse_ce)  # аналог np.mean для тензоров
+
+
+def accuracy(y_pred, y):
+    # Вычисляем точность после извлечения предсказаний класса
+    class_preds = tf.argmax(tf.nn.softmax(y_pred), axis=1)  # возвращает индекс максимального элемента
+    is_equal = tf.equal(y, class_preds)  # сравнение тензорных значений
+    return tf.reduce_mean(tf.cast(is_equal, tf.float32))  # аналог np.mean для тензоров
+
+
+class Adam:
+    def __init__(self, learning_rate=1e-3, beta_1=0.9, beta_2=0.999, ep=1e-7):
+        # Инициализируем параметры оптимизатора и резервируем переменные
+        self.beta_1 = beta_1  # гиперпараметр
+        self.beta_2 = beta_2  # гиперпараметр
+        self.learning_rate = learning_rate  # шаг обучения
+        self.ep = ep  # Эпсилон - предотвращает деление на ноль, слабо влияет на обучение
+        self.t = 1.
+        self.v_dvar, self.s_dvar = [], []
+        self.built = False
+
+    def apply_gradients(self, grads, vars):
+        # Инициализируем переменные при первом вызове
+        if not self.built:
+            for var in vars:
+                v = tf.Variable(tf.zeros(shape=var.shape))
+                s = tf.Variable(tf.zeros(shape=var.shape))
+                self.v_dvar.append(v)
+                self.s_dvar.append(s)
+            self.built = True
+        # Обновите переменные модели с учетом их градиентов
+        for i, (d_var, var) in enumerate(zip(grads, vars)):
+            self.v_dvar[i].assign(self.beta_1 * self.v_dvar[i] + (1 - self.beta_1) * d_var)
+            self.s_dvar[i].assign(self.beta_2 * self.s_dvar[i] + (1 - self.beta_2) * tf.square(d_var))
+            v_dvar_bc = self.v_dvar[i] / (1 - (self.beta_1 ** self.t))
+            s_dvar_bc = self.s_dvar[i] / (1 - (self.beta_2 ** self.t))
+            var.assign_sub(self.learning_rate * (v_dvar_bc / (tf.sqrt(s_dvar_bc) + self.ep)))
+        self.t += 1.
+        return
+
+
+def train_step(x_batch, y_batch, loss, acc, model, optimizer):
+    # Обновляем состояние модели с учетом пакета данных
+    with tf.GradientTape() as tape:
+        y_pred = model(x_batch)  # Предсказываем значение с текущими весами
+        batch_loss = loss(y_pred, y_batch)  # Оцениваем ошибку на текущих весах
+    batch_acc = acc(y_pred, y_batch)  # Оцениваем точность на текущих весах
+    grads = tape.gradient(batch_loss, model.variables)  # Вычисляем градиенты
+    optimizer.apply_gradients(grads, model.variables)  # Обновляем переменные с учетом полученных градиентов
+    return batch_loss, batch_acc  # Возвращаем ошибку и точность на основе заданного пакета
+
+
+def val_step(x_batch, y_batch, loss, acc, model):
+    # Оцениваем модель на основе заданного пакета валидационных данных без обучения на текущих весах
+    y_pred = model(x_batch)
+    batch_loss = loss(y_pred, y_batch)
+    batch_acc = acc(y_pred, y_batch)
+    return batch_loss, batch_acc
+
+
+def train_model(mlp, train_data, val_data, loss, acc, optimizer, epochs):
+    # Инициализация структур данных
+    train_losses, train_accs = [], []
+    val_losses, val_accs = [], []
+
+    # Отформатируем тренировочный цикл и начнем обучение
+    for epoch in range(epochs):
+        batch_losses_train, batch_accs_train = [], []
+        batch_losses_val, batch_accs_val = [], []
+
+        # Выполняем итерацию по обучающим данным
+        for x_batch, y_batch in train_data:
+            # Вычисляем градиенты и обновляем параметры модели
+            batch_loss, batch_acc = train_step(x_batch, y_batch, loss, acc, mlp, optimizer)
+            # Следим за результатами обучения на пакетном уровне
+            batch_losses_train.append(batch_loss)
+            batch_accs_train.append(batch_acc)
+
+        # Повторяем процедуру проверки данных
+        for x_batch, y_batch in val_data:
+            batch_loss, batch_acc = val_step(x_batch, y_batch, loss, acc, mlp)
+            batch_losses_val.append(batch_loss)
+            batch_accs_val.append(batch_acc)
+
+        # Следим за производительностью модели на уровне эпохи
+        train_loss, train_acc = tf.reduce_mean(batch_losses_train), tf.reduce_mean(batch_accs_train)
+        val_loss, val_acc = tf.reduce_mean(batch_losses_val), tf.reduce_mean(batch_accs_val)
+        train_losses.append(train_loss)
+        train_accs.append(train_acc)
+        val_losses.append(val_loss)
+        val_accs.append(val_acc)
+        print(f"Эпоха: {epoch}")
+        print(f"Обучающая ошибка: {train_loss:.3f}, Обучающая точность: {train_acc:.3f}")
+        print(f"Валидационная ошибка: {val_loss:.3f}, Валидационная точность: {val_acc:.3f}")
+    return train_losses, train_accs, val_losses, val_accs
